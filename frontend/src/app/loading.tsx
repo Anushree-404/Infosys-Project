79c1379783389d351ad95f53a4964f18bacfa2ef
Anushree D <anushreedhanashetti@gmail.com>
2026-08-03 17:54:08 +0530

    Delete frontend directory

--- frontend/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Global Loading UI (Next.js App Router)
- */
-
-import { PageLoader } from '@/components/ui/LoadingSpinner';
-
-export default function Loading() {
-  return <PageLoader />;
-}
